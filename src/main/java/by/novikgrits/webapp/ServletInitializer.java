2fe16b1a872d66567b4a827e2e56bed13726e593
NovikVeronika <veronika.novik79@mail.ru>
2022-09-13 19:36:20 +0300

    rename base package

--- src/main/java/by/novikgrits/webapp/ServletInitializer.java
+++ src/main/java/by/novikgrits/webapp/ServletInitializer.java
@@ -1,4 +1,4 @@
-package by.nivkgrits.webapp;
+package by.novikgrits.webapp;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
--- src/main/java/by/novikgrits/webapp/WebappApplication.java
+++ src/main/java/by/novikgrits/webapp/WebappApplication.java
@@ -1,4 +1,4 @@
-package by.nivkgrits.webapp;
+package by.novikgrits.webapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
