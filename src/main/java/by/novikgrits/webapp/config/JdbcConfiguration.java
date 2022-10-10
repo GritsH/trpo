5e4b0cd27291cc0cd813ff8167d19ce729c6e873
GritsH <grits.lena@gmail.com>
2022-10-10 19:17:47 +0300

    add jdbc configuration

---
+++ src/main/java/by/novikgrits/webapp/config/JdbcConfiguration.java
@@ -0,0 +1,37 @@
+package by.novikgrits.webapp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableJdbcRepositories("by.novikgrits.webapp")
+public class JdbcConfiguration {
+    @Bean
+    NamedParameterJdbcOperations operations() {
+        return new NamedParameterJdbcTemplate(dataSource());
+    }
+
+    @Bean
+    PlatformTransactionManager transactionManager() {
+        return new DataSourceTransactionManager(dataSource());
+    }
+
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/auction");
+        dataSource.setUsername("root"); //TODO change username if it differs
+        dataSource.setPassword("qw217746121"); //TODO change password if its differs
+        return dataSource;
+    }
+}
