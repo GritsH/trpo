db9291ec54266a3141806043d27ee0add1aedc07
GritsH <grits.lena@gmail.com>
2022-11-04 17:20:23 +0300

    add login controller

---
+++ src/main/java/by/novikgrits/webapp/controller/LogInController.java
@@ -0,0 +1,18 @@
+package by.novikgrits.webapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class LogInController {
+    @GetMapping("/login")
+    public String loginForm() {
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String loginPost() {
+        return "redirect:/main";
+    }
+}
