d642480b8cde40b3e6c359d51ce4c5d6a2dc3919
GritsH <grits.lena@gmail.com>
2022-11-04 17:18:25 +0300

    minor changes

--- src/main/java/by/novikgrits/webapp/model/User.java
+++ src/main/java/by/novikgrits/webapp/model/User.java
@@ -36,6 +36,9 @@ public class User {
         this.passportNumber = passportNumber;
     }
 
+    public User() {
+    }
+
     public Integer getId() {
         return id;
     }
--- src/main/java/by/novikgrits/webapp/model/repository/LotCategoryRepository.java
+++ src/main/java/by/novikgrits/webapp/model/repository/LotCategoryRepository.java
@@ -4,6 +4,9 @@ import by.novikgrits.webapp.model.LotCategory;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface LotCategoryRepository extends CrudRepository<LotCategory, Integer> {
+    Optional<LotCategory> findLotCategoryByCategoryName(String name);
 }
