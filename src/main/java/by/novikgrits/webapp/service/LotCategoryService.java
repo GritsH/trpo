114e67d4676886e5a9f5f711e582c3ff435c2159
GritsH <grits.lena@gmail.com>
2022-11-04 17:19:50 +0300

    add service for lot categories

---
+++ src/main/java/by/novikgrits/webapp/service/LotCategoryService.java
@@ -0,0 +1,24 @@
+package by.novikgrits.webapp.service;
+
+import by.novikgrits.webapp.model.LotCategory;
+import by.novikgrits.webapp.model.repository.LotCategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class LotCategoryService {
+    @Autowired
+    private LotCategoryRepository lotCategoryRepository;
+
+    public List<LotCategory> getAllCategories(){
+        return (List<LotCategory>) lotCategoryRepository.findAll();
+    }
+
+    public Optional<LotCategory> getByName(String name){
+        return lotCategoryRepository.findLotCategoryByCategoryName(name);
+    }
+
+}
