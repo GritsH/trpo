71ce6eab519438ff5a2cc3ae22e40037e3ad27b1
GritsH <grits.lena@gmail.com>
2022-11-15 20:37:16 +0300

    minor fixes

--- src/test/java/by/novikgrits/webapp/service/LotServiceTest.java
+++ src/test/java/by/novikgrits/webapp/service/LotServiceTest.java
@@ -32,8 +32,8 @@ class LotServiceTest {
     @DisplayName("should add new lot")
     @Test
     void addLot() {
-        Lot lot = new Lot(1, LocalDate.now(), LocalDate.now(), 1.0, 1.0, 1, 1);
-
+        Lot lot = new Lot(1, LocalDate.now(), LocalDate.now(),
+                1.0, 1.0, 1.0, 1, 1, "info", "name");
         lotService.addLot(lot);
 
         verify(lotRepository).save(lot);
