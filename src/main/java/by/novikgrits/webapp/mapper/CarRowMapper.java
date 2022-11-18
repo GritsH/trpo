49d15abf9cc1f7d44c46f28b1b89a81d64efd241
GritsH <grits.lena@gmail.com>
2022-11-18 11:30:14 +0300

    add car + real estate repos

---
+++ src/main/java/by/novikgrits/webapp/mapper/CarRowMapper.java
@@ -0,0 +1,23 @@
+package by.novikgrits.webapp.mapper;
+
+import by.novikgrits.webapp.model.Car;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class CarRowMapper implements RowMapper<Car> {
+    @Override
+    public Car mapRow(ResultSet rs, int rowNum) throws SQLException {
+        Car car = new Car();
+
+        car.setId(rs.getInt("id"));
+        car.setBrand(rs.getString("brand"));
+        car.setMileage(rs.getInt("mileage"));
+        car.setEngineVolume(rs.getInt("engine_volume"));
+        car.setManufactureYear(rs.getDate("manufacture_year"));
+        car.setLotId(rs.getInt("lot_id"));
+
+        return car;
+    }
+}
---
+++ src/main/java/by/novikgrits/webapp/repository/CarRepository.java
@@ -0,0 +1,45 @@
+package by.novikgrits.webapp.repository;
+
+import by.novikgrits.webapp.mapper.CarRowMapper;
+import by.novikgrits.webapp.model.Car;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class CarRepository {
+    private static final String INSERT = "insert into car (lot_id, brand, manufacture_year, mileage, engine_volume) values (?,?,?,?,?)";
+    private static final String SELECT_BY_LOT_ID = "select * from car where lot_id = ?";
+    private static final String SELECT_ALL = "select * from car";
+    private static final String DELETE_BY_ID = "delete from car where id = ?";
+    private static final String DELETE_BY_LOT_ID = "delete from car where lot_id = ?";
+    private final JdbcTemplate jdbcTemplate;
+
+    public CarRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public void save(Car car){
+        jdbcTemplate.update(INSERT,
+                car.getLotId(), car.getBrand(), car.getManufactureYear(), car.getMileage(), car.getEngineVolume());
+    }
+
+    public Optional<Car> findByLotId(Integer lotId){
+        return Optional.ofNullable(jdbcTemplate.queryForObject(SELECT_BY_LOT_ID, new Object[]{lotId}, new CarRowMapper()));
+    }
+
+    public List<Car> findAll(){
+        List<Car> allCars = jdbcTemplate.query(SELECT_ALL, new CarRowMapper());
+        return allCars;
+    }
+
+    public void deleteById(Integer carId){
+        jdbcTemplate.update(DELETE_BY_ID, carId);
+    }
+
+    public void deleteByLotId(Integer lotId){
+        jdbcTemplate.update(DELETE_BY_LOT_ID, lotId);
+    }
+}
