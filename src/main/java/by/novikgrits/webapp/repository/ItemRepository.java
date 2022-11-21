c3cbed11e912b107c7add05a0bf281e4c9bf27b5
GritsH <grits.lena@gmail.com>
2022-11-21 17:03:44 +0300

    changes in auction classes

---
+++ src/main/java/by/novikgrits/webapp/repository/ItemRepository.java
@@ -0,0 +1,15 @@
+package by.novikgrits.webapp.repository;
+
+import by.novikgrits.webapp.model.item.Item;
+import by.novikgrits.webapp.model.item.ItemType;
+
+import java.util.Optional;
+
+public interface ItemRepository {
+
+    void save(Item item);
+
+    Optional<Item> findByLotId(Integer id);
+
+    ItemType getType();
+}
---
+++ src/main/java/by/novikgrits/webapp/repository/item/ItemRepositoryProvider.java
@@ -0,0 +1,25 @@
+package by.novikgrits.webapp.repository.item;
+
+import by.novikgrits.webapp.model.item.ItemType;
+import by.novikgrits.webapp.repository.ItemRepository;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+@Component
+public class ItemRepositoryProvider {
+
+    private final Map<ItemType, ItemRepository> itemReposByType;
+
+    public ItemRepositoryProvider(List<ItemRepository> itemRepositories) {
+        this.itemReposByType = itemRepositories.stream()
+                .collect(Collectors.toMap(ItemRepository::getType, Function.identity()));
+    }
+
+    public ItemRepository findRepoByType(ItemType itemType) {
+        return itemReposByType.get(itemType);
+    }
+}
