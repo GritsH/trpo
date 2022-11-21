fcd27f8b9dc819eef1ec803b3fa3e3471422a5a4
GritsH <grits.lena@gmail.com>
2022-11-21 17:05:19 +0300

    fixes in CategoriesController

--- src/main/java/by/novikgrits/webapp/controller/CategoriesController.java
+++ src/main/java/by/novikgrits/webapp/controller/CategoriesController.java
@@ -1,25 +1,23 @@
 package by.novikgrits.webapp.controller;
 
 import by.novikgrits.webapp.model.ItemCategory;
+import by.novikgrits.webapp.model.item.ItemType;
 import by.novikgrits.webapp.service.ItemCategoryService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class CategoriesController {
-    private final ItemCategoryService itemCategoryService;
-
-    public CategoriesController(ItemCategoryService itemCategoryService) {
-        this.itemCategoryService = itemCategoryService;
-    }
 
     @GetMapping("/categories")
     public String getCategories(Model model) {
-        List<ItemCategory> allCategories = itemCategoryService.getAllCategories();
-        model.addAttribute("all_categories", allCategories);
+        List<String> allCategories = new ArrayList<>();
+        ItemType.stream().forEach(i -> allCategories.add(i.getTypeDescription()));
+        model.addAttribute("categories", allCategories);        model.addAttribute("all_categories", allCategories);
         return "categories";
     }
 }
