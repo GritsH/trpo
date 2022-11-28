3c08cfa6e989b317274e4216fe52b56aaac0ba91
GritsH <grits.lena@gmail.com>
2022-11-28 21:31:41 +0300

    add medical speciality

--- src/main/java/by/novikgrits/webapp/model/item/MedicalSpeciality.java
+++ src/main/java/by/novikgrits/webapp/model/item/MedicalSpeciality.java
@@ -1,4 +1,36 @@
 package by.novikgrits.webapp.model.item;
 
+import java.util.Objects;
+import java.util.stream.Stream;
+
 public enum MedicalSpeciality {
+    DENTISTRY("dentistry"),
+    GYNECOLOGY("gynecology"),
+    ULTRASOUND("ultrasound"),
+    CONTRAST_TOMOGRAPHY("contrast tomography"),
+    OPHTHALMOLOGY("ophthalmology"),
+    OTHER("other");
+
+    private String typeDescription;
+
+    public String getTypeDescription() {
+        return typeDescription;
+    }
+
+    MedicalSpeciality(String typeDescription) {
+        this.typeDescription = typeDescription;
+    }
+
+    public static Stream<MedicalSpeciality> stream() {
+        return Stream.of(MedicalSpeciality.values());
+    }
+
+    public static MedicalSpeciality getByName(String description) {
+        for (MedicalSpeciality value : MedicalSpeciality.values()) {
+            if (Objects.equals(value.typeDescription, description)) {
+                return value;
+            }
+        }
+        return null;
+    }
 }
