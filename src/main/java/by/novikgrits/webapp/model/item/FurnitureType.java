bd2729f2e648c5523447d5ed2979dfb71e2e7bb3
GritsH <grits.lena@gmail.com>
2022-11-28 21:30:55 +0300

    add furniture type

--- src/main/java/by/novikgrits/webapp/model/item/FurnitureType.java
+++ src/main/java/by/novikgrits/webapp/model/item/FurnitureType.java
@@ -1,4 +1,40 @@
 package by.novikgrits.webapp.model.item;
 
+import java.util.Objects;
+import java.util.stream.Stream;
+
 public enum FurnitureType {
+    CHAIR ("chair"),
+    TABLE("table"),
+    BED("bed"),
+    CUPBOARD("cupboard"),
+    WARDROBE("wardrobe"),
+    LAMP("lamp"),
+    SOFA("sofa"),
+    ARMCHAIR("armchair"),
+    OTHER("other");
+
+    private String typeDescription;
+
+    public String getTypeDescription() {
+        return typeDescription;
+    }
+
+    FurnitureType(String typeDescription) {
+        this.typeDescription = typeDescription;
+    }
+
+    public static Stream<FurnitureType> stream() {
+        return Stream.of(FurnitureType.values());
+    }
+
+    public static FurnitureType getByName(String description) {
+        for (FurnitureType value : FurnitureType.values()) {
+            if (Objects.equals(value.typeDescription, description)) {
+                return value;
+            }
+        }
+        return null;
+    }
+
 }
