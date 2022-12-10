46f72aacdea8867b8117915f32c987468a2dd28d
NovikVeronika <63884254+NovikVeronika@users.noreply.github.com>
2022-12-10 09:22:16 +0300

    Frontend pages: more js (#20)
    * add some thymeleaf
    
    * add thymeleaf for admin user id and admin lot id
    
    * fix thymeleaf for active auctions and profile pages
    
    * add thymeleaf for admin main without table
    
    * add minor fixes
    
    * add thymeleaf for lot and lot-details
    
    * add some fixes
    
    * add some changes
    
    * add some fixes
    
    * add some fixes
    
    * add thymeleaf for all categories pages
    
    * add some changes
    
    * add some changes
    
    * add dropdown and radiobuttons
    
    * add images
    
    * Revert "add images"
    
    This reverts commit a538a9060749e722afda2a165df6e225638bc2e7.
    
    * add images
    
    * add js for signup and login
    
    * add image input and fixes for js
    
    * add js for some pages
    
    * add changes for admin and creation pages
    
    * add error pages
    
    Co-authored-by: GritsH <grits.lena@gmail.com>

--- src/main/resources/static/js/validation.js
+++ src/main/resources/static/js/validation.js
@@ -1,6 +1,6 @@
 export function validateEmail(email) {
     const regex = /(?:[a-z\d!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z\d!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z\d](?:[a-z\d-]*[a-z\d])?\.)+[a-z\d](?:[a-z\d-]*[a-z\d])?|\[(?:(?:25[0-5]|2[0-4]\d|[01]?\d\d?)\.){3}(?:25[0-5]|2[0-4]\d|[01]?\d\d?|[a-z\d-]*[a-z\d]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)])/;
-    return regex.test(email);
+    return email.match(regex);
 }
 
 export function validatePassword(password) {
@@ -9,7 +9,7 @@ export function validatePassword(password) {
 
 export function validatePhoneNumber(phoneNumber) {
     const regexPhone = /^[+]?[0-9]{3}?[-\s\.]?[0-9]{2}[-\s\.]?[0-9]{7}$/;
-    return regexPhone.test(phoneNumber);
+    return phoneNumber.match(regexPhone);
 }
 
 export function validatePassport(passport) {
@@ -17,4 +17,14 @@ export function validatePassport(passport) {
     const regexPassport  = /^[A-Z]{2}\d{7}$/;
 
     return passport.match(regexPassport);
+}
+
+export function validateCarEngineVolume(carEngineVolume) {
+    const regexEngineVolume = /^[0-9]+(\.[0-9]{1,2})$/;
+    return carEngineVolume.match(regexEngineVolume);
+}
+
+export function validateManufactureYear(manufactureYear) {
+    const regexManufactureYear = /^(19[4-9]\d|20[0-2]\d|2022)$/;
+    return manufactureYear.match(regexManufactureYear);
 }
